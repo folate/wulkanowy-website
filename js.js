a073223119a5c9bd14d4da81bda7f1c382f5a105
folate <52334543+folate@users.noreply.github.com>
2021-12-29 13:57:10 +0100

    Under the hood changes
    Under the hood changes

--- js.js
+++ js.js
@@ -10,31 +10,12 @@ window.addEventListener('scroll', function () {
     var dim = position.height / 8;
 
     if (elmpos >= dim) {
-        $("nav").css({'background-color': 'rgba(22, 22, 22, 1)', 'border-bottom': '1px solid rgba(177, 177, 177, 0.2)', 'color': 'rgba(255, 255, 255, 1)', 'fill': 'rgba(255, 255, 255, 1)'});
-        $(".nav-items ul li a, .nav-items .logo a svg g, .nav-items .logo a svg g path, .nav-items .logo a svg path, .nav-items .logo a svg, .nav-items .logo a p").css({'fill': 'rgba(255, 255, 255, 1)', });
+        $(".navbar").addClass("scrolled");
     } 
     else {
-        $("nav").css({'background-color': 'rgba(22, 22, 22, 0)', 'border-bottom': '1px solid rgba(177, 177, 177, 0)'});
-        $(".nav-items ul li a, .nav-items .logo a svg g, .nav-items .logo a svg g path, .nav-items .logo a svg path, .nav-items .logo a svg, .nav-items .logo a p").css({'fill': 'rgba(255, 255, 255, 1)', });
+        $(".navbar").removeClass("scrolled");
     }
 });
-
-// collapsable table shit..
-//var coll = document.getElementsByClassName("collapse-table");
-//var i;
-
-//for (i = 0; i < coll.length; i++) {
-//  coll[i].addEventListener("click", function() {
-//    this.classList.toggle("active");
-//    var content = this.nextElementSibling;
-//    if (content.style.maxHeight){
-//      content.style.maxHeight = null;
-//    } else {
-//      content.style.maxHeight = content.scrollHeight + "px";
-//    } 
-//  });
-//}
-
 
 function GetLatestReleaseInfo() {
   $.getJSON("https://api.github.com/repos/wulkanowy/wulkanowy/releases/latest").done(function(release) {
